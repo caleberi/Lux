b5eb6cda883eaefed7a664c330a22fc0e3041955
caleberi <caleberioluwa@gmail.com>
2024-12-20 17:32:45 +0100

    feat: support logical expression, function call expression

--- src/main/java/com/interpreters/lox/Expr.java
+++ src/main/java/com/interpreters/lox/Expr.java
@@ -1,12 +1,18 @@
 package com.interpreters.lox;
 
+import java.util.List;
+
 abstract class Expr {
   interface Visitor<R> {
     R visitBinaryExpr(Binary expr);
 
+    R visitCallExpr(Call expr);
+
     R visitGroupingExpr(Grouping expr);
 
     R visitLiteralExpr(Literal expr);
+
+    R visitLogicalExpr(Logical expr);
 
     R visitUnaryExpr(Unary expr);
 
@@ -34,6 +40,23 @@ abstract class Expr {
     }
   }
 
+  static class Call extends Expr {
+    Call(Expr callee, Token paren, List<Expr> arguments) {
+      this.callee = callee;
+      this.paren = paren;
+      this.arguments = arguments;
+    }
+
+    final Expr callee;
+    final Token paren;
+    final List<Expr> arguments;
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitCallExpr(this);
+    }
+  }
+
   static class Grouping extends Expr {
     Grouping(Expr expression) {
       this.expression = expression;
@@ -57,6 +80,23 @@ abstract class Expr {
     @Override
     <R> R accept(Visitor<R> visitor) {
       return visitor.visitLiteralExpr(this);
+    }
+  }
+
+  static class Logical extends Expr {
+    Logical(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLogicalExpr(this);
     }
   }
 
