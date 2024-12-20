5b25716e6653ca8b74a7af0a97ee8b3cf2a53c76
caleberi <caleberioluwa@gmail.com>
2024-12-20 17:38:26 +0100

    feat: add function ast evalutation support

--- src/main/java/com/interpreters/lox/LoxCallable.java
+++ src/main/java/com/interpreters/lox/LoxCallable.java
@@ -1,2 +1,8 @@
-package com.interpreters.lox;public interface LoxCallable {
+package com.interpreters.lox;
+
+import java.util.List;
+
+public interface LoxCallable {
+    int arity();
+    Object call(Interpreter interpreter, List<Object> arguments);
 }
--- src/main/java/com/interpreters/lox/LoxFunction.java
+++ src/main/java/com/interpreters/lox/LoxFunction.java
@@ -1,2 +1,36 @@
-package com.interpreters.lox;public class LoxFunction {
+package com.interpreters.lox;
+
+import java.util.List;
+
+public class LoxFunction implements  LoxCallable{
+    private final Stmt.Function declaration;
+    private final Environment closure;
+    LoxFunction(Stmt.Function declaration, Environment closure){
+        this.declaration = declaration;
+        this.closure = closure;
+    }
+    @Override
+    public int arity() {
+        return this.declaration.parameters.size();
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        Environment environment = new Environment(closure);
+        for (int i = 0; i < declaration.parameters.size(); i++)
+            environment.define(declaration.parameters.get(i).lexeme,
+                    arguments.get(i));
+
+       try {
+           interpreter.executeBlock(declaration.body, environment);
+       } catch (Return returnValue){
+           return  returnValue.value;
+       }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "<fx "+this.declaration.functionName+ ">";
+    }
 }
