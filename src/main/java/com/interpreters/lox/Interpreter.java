846b1c1203d2cc687e194668fd29a4076d0afbb3
caleberi <caleberioluwa@gmail.com>
2025-01-04 05:41:08 +0100

    Feat: include support for super class.

--- src/main/java/com/interpreters/lox/Interpreter.java
+++ src/main/java/com/interpreters/lox/Interpreter.java
@@ -201,6 +201,15 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitClassStmt(Stmt.Class stmt) {
+
+        Object superclass = null;
+        if (stmt.superclass != null) {
+            superclass = evaluate(stmt.superclass);
+            if (!(superclass instanceof LoxClass)) {
+                throw new RuntimeError(stmt.superclass.name, "Superclass must be class.");
+            }
+        }
+
         environment.define(stmt.name.lexeme,null);
         Map<String, LoxFunction> methods = new HashMap<>();
         for (Stmt.Function method : stmt.methods) {
@@ -208,7 +217,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
                     method.functionName.lexeme.equals("init"));
             methods.put(method.functionName.lexeme,function);
         }
-        LoxClass klass = new LoxClass(stmt.name.lexeme,methods);
+        LoxClass klass = new LoxClass(stmt.name.lexeme, (LoxClass) superclass,methods);
         environment.assign(stmt.name,klass);
         return null;
     }
