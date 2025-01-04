9a82431c79196bfa350d448061bfc682f8075ee0
caleberi <caleberioluwa@gmail.com>
2025-01-04 10:42:36 +0100

    Feat: make enclosing public

--- src/main/java/com/interpreters/lox/Environment.java
+++ src/main/java/com/interpreters/lox/Environment.java
@@ -5,12 +5,8 @@ import java.util.Map;
 
 public class Environment {
 
-    private final Environment enclosing;
+    final Environment enclosing;
     private final Map<String,Object> values = new HashMap<>();
-
-    Environment() {
-        this.enclosing = null;
-    }
 
     Environment(Environment enclosing){
         this.enclosing =  enclosing;
